b96c3556c9fe44dd63d65af6a66a13698784e3a0
hmon <wtthumon@gmail.com>
2024-05-17 10:12:49 +0200

    carousell works

--- main.js
+++ main.js
@@ -1,3 +1,57 @@
-letter = document.querySelector(".letter");
-console.log(letter);
-letter.addEventListener("click", () => console.log("clicked"));
+window.onload = function () {
+  const carousel = document.querySelector(".carousel");
+
+  for (let i = 0; i < 8; i++) {
+    const img_li = document.createElement("li");
+    const img = document.createElement("img");
+
+    img.src = `./assets/images/${i + 1}.jpeg`;
+    img_li.appendChild(img);
+    carousel.appendChild(img_li);
+  }
+
+  const previousButton = document.querySelector("#left");
+  const nextButton = document.querySelector("#right");
+
+  function setSlidePoisition(slide, index) {
+    slides[index].style.left = 100 * index + "vw";
+    slides[index].classList.add("slide");
+    slides[index].setAttribute("id", index);
+    if (index == 0) {
+      slides[index].classList.add("current-slide");
+    }
+  }
+
+  const slides = Array.from(carousel.children);
+  slides.forEach(setSlidePoisition);
+
+  const moveSlide = (carousel, currentSlide, targetSlide) => {
+    carousel.style.transform = "translateX(-" + targetSlide.style.left + ")";
+    currentSlide.classList.remove("current-slide");
+    targetSlide.classList.add("current-slide");
+  };
+
+  nextButton.addEventListener("click", () => {
+    const currentSlide = carousel.querySelector(".current-slide");
+    let targetSlide;
+    const id = currentSlide.getAttribute("id");
+    if (id != slides.length - 1) {
+      targetSlide = currentSlide.nextElementSibling;
+    } else {
+      targetSlide = slides[0];
+    }
+    moveSlide(carousel, currentSlide, targetSlide);
+  });
+
+  previousButton.addEventListener("click", () => {
+    const currentSlide = carousel.querySelector(".current-slide");
+    let targetSlide;
+    const id = currentSlide.getAttribute("id");
+    if (id != 0) {
+      targetSlide = currentSlide.previousElementSibling;
+    } else {
+      targetSlide = slides[slides.length - 1];
+    }
+    moveSlide(carousel, currentSlide, targetSlide);
+  });
+};
